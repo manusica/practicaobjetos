10b920d57d91619418b0922a61966cc81ec130ea
manuelsica <manuelsicardi0@gmail.com>
2024-10-15 11:04:28 -0300

    gurdado

---
+++ ejercicio1.js
@@ -0,0 +1,61 @@
+const alumno1 = {
+    nombre: "mario",
+    apellido: "pedernera",
+    edad: 17,
+    tecnicatura: "informatica",
+    año:"5to",
+    telefono:3465695879
+}
+const alumno2 = {
+    nombre: "manu",
+    apellido: "sica",
+    edad: 17,
+    tecnicatura: "informatica",
+    año:"5to"
+}
+const alumno3 = {
+    nombre: "bruno",
+    apellido: "ocano",
+    edad: 17,
+    tecnicatura: "informatica",
+    año:"5to"
+}
+const alumno4 = {
+    nombre: "stefano",
+    apellido: "pinari",
+    edad: 19,
+    tecnicatura: "informatica",
+    año:"6to"
+}
+const alumno5 = {
+    nombre: "tomas",
+    apellido: "hernandez",
+    edad: 19,
+    tecnicatura: "informatica",
+    año:"6to"
+}
+const alumno6 = {
+    nombre: "laureano",
+    apellido: "corti",
+    edad: 19,
+    tecnicatura: "informatica",
+    año:"6to"
+}
+const alumno7 = {
+    nombre: "santiago",
+    apellido: "silva",
+    edad: 17,
+    tecnicatura: "electromecanica",
+    año:"5to"
+}
+const alumno8 = {
+    nombre: "mateo",
+    apellido: "silva",
+    edad: 17,
+    tecnicatura: "electromecanica",
+    año:"5to"
+}
+
+console.log("los datos registrados del objeto 1 son", alumno1)
+alumno1.nombre = "Mariano";
+console.log("los datos actualizados del objeto 1 son", alumno1.nombre)
